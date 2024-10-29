f80fcbf276fee38ac5a76b751902cdd32a3ce882
ggeo <geovannesilvadejesus13@gmail.com>
2024-10-29 14:23:56 -0300

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,14 +1,5 @@
-fstrim -v /data       
-# User data partition 
-fstrim -v /cache      
-# Cache partition 
-fstrim -v /system     
-# System partition (some systems may not allow this) 
-fstrim -v /vendor #vendor partition 
+fstrim -v /data
+fstrim -v /cache
+fstrim -v /system
+fstrim -v /vendor
 fstrim -v /mnt/vendor
-# some phones may not allow this 
-# this command will generate logs in your internal storage:
-fstrim -v /data >> /sdcard/fstrim_log.txt
-fstrim -v /cache >> /sdcard/fstrim_log.txt
-fstrim -v /system >> /sdcard/fstrim_log.txt
-# they are just useless logs
